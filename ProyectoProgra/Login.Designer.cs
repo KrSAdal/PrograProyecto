776980e4a6a04bd87e4a561258697e226432bc5e
wander1405 <alexander.cabnal.1405@gmail.com>
2025-05-06 09:21:12 -0600

    login

--- ProyectoProgra/Login.Designer.cs
+++ ProyectoProgra/Login.Designer.cs
@@ -28,26 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtUsuario = new System.Windows.Forms.TextBox();
-            this.txtContrasena = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsuario = new MaterialSkin.Controls.MaterialTextBox2();
+            this.txtContrasena = new MaterialSkin.Controls.MaterialTextBox2();
+            this.btnLogin = new MaterialSkin.Controls.MaterialButton();
             this.SuspendLayout();
-            // 
-            // txtUsuario
-            // 
-            this.txtUsuario.Location = new System.Drawing.Point(267, 65);
-            this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(100, 22);
-            this.txtUsuario.TabIndex = 9;
-            // 
-            // txtContrasena
-            // 
-            this.txtContrasena.Location = new System.Drawing.Point(267, 118);
-            this.txtContrasena.Name = "txtContrasena";
-            this.txtContrasena.Size = new System.Drawing.Size(100, 22);
-            this.txtContrasena.TabIndex = 8;
             // 
             // label2
             // 
@@ -61,32 +47,98 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(64, 118);
+            this.label1.Location = new System.Drawing.Point(64, 125);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 16);
             this.label1.TabIndex = 6;
             this.label1.Text = "Contraseña";
             // 
+            // txtUsuario
+            // 
+            this.txtUsuario.AnimateReadOnly = false;
+            this.txtUsuario.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.txtUsuario.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.txtUsuario.Depth = 0;
+            this.txtUsuario.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtUsuario.HideSelection = true;
+            this.txtUsuario.LeadingIcon = null;
+            this.txtUsuario.Location = new System.Drawing.Point(288, 80);
+            this.txtUsuario.MaxLength = 32767;
+            this.txtUsuario.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.PasswordChar = '\0';
+            this.txtUsuario.PrefixSuffixText = null;
+            this.txtUsuario.ReadOnly = false;
+            this.txtUsuario.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtUsuario.SelectedText = "";
+            this.txtUsuario.SelectionLength = 0;
+            this.txtUsuario.SelectionStart = 0;
+            this.txtUsuario.ShortcutsEnabled = true;
+            this.txtUsuario.Size = new System.Drawing.Size(250, 48);
+            this.txtUsuario.TabIndex = 10;
+            this.txtUsuario.TabStop = false;
+            this.txtUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtUsuario.TrailingIcon = null;
+            this.txtUsuario.UseSystemPasswordChar = false;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.AnimateReadOnly = false;
+            this.txtContrasena.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.txtContrasena.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.txtContrasena.Depth = 0;
+            this.txtContrasena.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtContrasena.HideSelection = true;
+            this.txtContrasena.LeadingIcon = null;
+            this.txtContrasena.Location = new System.Drawing.Point(288, 140);
+            this.txtContrasena.MaxLength = 32767;
+            this.txtContrasena.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.PasswordChar = '\0';
+            this.txtContrasena.PrefixSuffixText = null;
+            this.txtContrasena.ReadOnly = false;
+            this.txtContrasena.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtContrasena.SelectedText = "";
+            this.txtContrasena.SelectionLength = 0;
+            this.txtContrasena.SelectionStart = 0;
+            this.txtContrasena.ShortcutsEnabled = true;
+            this.txtContrasena.Size = new System.Drawing.Size(250, 48);
+            this.txtContrasena.TabIndex = 11;
+            this.txtContrasena.TabStop = false;
+            this.txtContrasena.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtContrasena.TrailingIcon = null;
+            this.txtContrasena.UseSystemPasswordChar = false;
+            // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(65, 169);
+            this.btnLogin.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnLogin.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnLogin.Depth = 0;
+            this.btnLogin.HighEmphasis = true;
+            this.btnLogin.Icon = null;
+            this.btnLogin.Location = new System.Drawing.Point(288, 210);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnLogin.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(90, 33);
-            this.btnLogin.TabIndex = 5;
-            this.btnLogin.Text = "Login";
+            this.btnLogin.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnLogin.Size = new System.Drawing.Size(158, 36);
+            this.btnLogin.TabIndex = 12;
+            this.btnLogin.Text = "Ingresar";
+            this.btnLogin.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnLogin.UseAccentColor = false;
             this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click_1);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnLogin);
             this.Name = "Login";
             this.Text = "Form6";
             this.Load += new System.EventHandler(this.Login_Load);
@@ -96,11 +148,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtUsuario;
-        private System.Windows.Forms.TextBox txtContrasena;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnLogin;
+        private MaterialSkin.Controls.MaterialTextBox2 txtUsuario;
+        private MaterialSkin.Controls.MaterialTextBox2 txtContrasena;
+        private MaterialSkin.Controls.MaterialButton btnLogin;
     }
 }
--- ProyectoProgra/Login.cs
+++ ProyectoProgra/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace ProyectoProgra
 {
-    public partial class Login : Form
+    public partial class Login : MaterialForm
     {
         public Login()
         {
             InitializeComponent();
+
+            // Configuración de MaterialSkin con tema oscuro
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Indigo500, Primary.Indigo700,
+                Primary.Indigo100, Accent.Indigo200,
+                TextShade.WHITE
+            );
+
         }
 
         private void Login_Load(object sender, EventArgs e)
@@ -22,7 +36,7 @@ namespace ProyectoProgra
 
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
+        private void btnLogin_Click_1(object sender, EventArgs e)
         {
             string usuario = txtUsuario.Text;
             string contrasena = txtContrasena.Text;
